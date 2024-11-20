cb39ed25dc1de7729a7fb60409cd05e7687cba01
Pablo Benzo <>
2024-11-20 04:36:42 -0300

    Project files and signin settings.

---
+++ Cities/CitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesApp.swift
+//  Cities
+//
+//  Created by Pablo Benzo on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CitiesTests/CitiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesTests.swift
+//  CitiesTests
+//
+//  Created by Pablo Benzo on 20/11/2024.
+//
+
+import Testing
+@testable import Cities
+
+struct CitiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
