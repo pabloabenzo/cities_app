4f3cfcdef34fa504ff48dd525b914bae6b7e3a45
Pablo Benzo <>
2024-11-22 02:59:45 -0300

    Favorites logic and UI.

--- Cities/ListOfCities/ListOfCitiesViewModel.swift
+++ Cities/ListOfCities/ListOfCitiesViewModel.swift
@@ -6,12 +6,17 @@
 //
 
 import Foundation
+import SwiftUI
 import Observation
 
 @Observable
 class ListOfCitiesViewModel {
     
+    var initialCitiesLoaded: [CitiesInfo] = []
+    private var showingFavs = false
+    private var savedItems: Set<Int> = [1, 7]
     var listResults = [CitiesInfo]()
+    
     let url = URL(string: "https://gist.githubusercontent.com/hernan-uala/dce8843a8edbe0b0018b32e137bc2b3a/raw/0996accf70cb0ca0e16f9a99e0ee185fafca7af1/cities.json")
     
     func fetchData(from url: URL, completion: ((Result<Data, any Error>) -> Void)? = nil) async {
@@ -29,5 +34,44 @@ class ListOfCitiesViewModel {
         }
     }
     
+    func save(items: Set<Int>) {
+        let array = Array(items)
+        UserDefaults.standard.set(array, forKey: "fav_key")
+    }
     
+    func load() -> Set<Int> {
+        let array = UserDefaults.standard.array(forKey: "fav_key") as? [Int] ?? [Int]()
+        return Set(array)
+    }
+    
+    // Filter saved items
+    var filteredItems: [CitiesInfo]  {
+        if showingFavs {
+            return initialCitiesLoaded.filter { savedItems.contains($0._id) }
+        }
+        return initialCitiesLoaded
+    }
+        
+    init() {
+        self.savedItems = load()
+    }
+    
+    func sortFavs() {
+        withAnimation() {
+            showingFavs.toggle()
+        }
+    }
+    
+    func contains(_ item: CitiesInfo) -> Bool {
+            savedItems.contains(item._id)
+        }
+    
+    func toggleFav(item: CitiesInfo) {
+        if contains(item) {
+            savedItems.remove(item._id)
+        } else {
+            savedItems.insert(item._id)
+        }
+        save(items: savedItems)
+    }
 }
--- Cities/ListOfCities/View/ListOfCitiesView.swift
+++ Cities/ListOfCities/View/ListOfCitiesView.swift
@@ -10,23 +10,17 @@ import SwiftUI
 struct ListOfCitiesView: View {
     
     private var citiesVM = ListOfCitiesViewModel()
-    @State private var initialCitiesLoaded: [CitiesInfo] = []
     @State private var isLoading: Bool = false
     @State private var isFavorite: Bool = false
     @State private var searchBar = ""
     @State private var isSheetViewPresented = false
     private var deviceWidth = UIScreen.main.bounds.width
     
-    var filteredCities: [CitiesInfo] {
-        guard !searchBar.isEmpty else { return citiesVM.listResults }
-        return citiesVM.listResults.filter { $0.name.lowercased().hasPrefix(searchBar.lowercased()) }
-    }
-    
     var body: some View {
         NavigationStack {
             ScrollView {
                 LazyVStack(alignment: .leading) {
-                    ForEach(searchBar.isEmpty ? initialCitiesLoaded : filteredCities, id: \.name) { city in
+                    ForEach(filteredSearch, id: \.name) { city in
                         HStack(spacing: deviceWidth / 20) {
                             NavigationLink(destination: CityMapView(listResult: city)) {
                                 VStack(alignment: .leading) {
@@ -37,6 +31,7 @@ struct ListOfCitiesView: View {
                                     Text("Lon: \(city.coord["lon"]!), Lat: \(city.coord["lat"]!)")
                                         .tint(Color.indigo)
                                 }
+                                .padding(.leading, 10)
                             }
                             .frame(minWidth: deviceWidth / 1.5)
                             .onAppear {
@@ -50,19 +45,32 @@ struct ListOfCitiesView: View {
                                     .resizable()
                                     .tint(Color.purple)
                                     .frame(width: 25, height: 25)
-                                    .padding(.leading, 10)
-                            }
-                            
-                            Button {
-                                isFavorite.toggle()
-                            } label: {
-                                Image(systemName: isFavorite ? "star.fill" : "star")
-                                    .foregroundColor(isFavorite ? .yellow : .gray)
                             }
                             .padding(.leading, 10)
                             
+                            Image(systemName: citiesVM.contains(city) ? "heart.fill" : "heart")
+                                .foregroundColor(.red)
+                                .onTapGesture {
+                                    citiesVM.toggleFav(item: city)
+                                }
+                                .padding(.leading, 10)
+                            
                         }
                         .padding(.bottom, 20).padding(.leading, 10)
+                    }
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: {
+                            citiesVM.sortFavs()
+                        }) {
+                            HStack {
+                                Image(systemName: "heart.fill")
+                                    .resizable()
+                                    .frame(width: 30, height: 25)
+                                    .tint(Color.red)
+                            }
+                        }
                     }
                 }
                 .sheet(isPresented: $isSheetViewPresented) {
@@ -88,32 +96,44 @@ struct ListOfCitiesView: View {
         
     }
     
+    var filteredSearch: [CitiesInfo]  {
+        if searchBar.isEmpty {
+            return citiesVM.filteredItems
+        }
+        return filteredCities
+    }
+    
+    var filteredCities: [CitiesInfo] {
+        guard !searchBar.isEmpty else { return citiesVM.listResults }
+        return citiesVM.listResults.filter { $0.name.lowercased().hasPrefix(searchBar.lowercased()) }
+    }
+    
     private func loadInitialCities() {
         let initialBatch = Array(citiesVM.listResults.prefix(20))
-        initialCitiesLoaded.append(contentsOf: initialBatch)
+        citiesVM.initialCitiesLoaded.append(contentsOf: initialBatch)
     }
     
     private func loadMoreContentIfNeeded(city: CitiesInfo) {
         
-        guard let lastCity = initialCitiesLoaded.last, city == lastCity else {
+        guard let lastCity = citiesVM.initialCitiesLoaded.last, city == lastCity else {
             return
         }
         
-        if !isLoading && initialCitiesLoaded.count < citiesVM.listResults.count {
+        if !isLoading && citiesVM.initialCitiesLoaded.count < citiesVM.listResults.count {
             isLoading = true
             
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                 
-                let nextBatchStart = initialCitiesLoaded.count
-                let nextBatchEnd = min(initialCitiesLoaded.count + 20, filteredCities.count)
+                let nextBatchStart = citiesVM.initialCitiesLoaded.count
+                let nextBatchEnd = min(citiesVM.initialCitiesLoaded.count + 20, filteredCities.count)
                 let nextBatch = filteredCities[nextBatchStart..<nextBatchEnd]
-                initialCitiesLoaded.append(contentsOf: nextBatch)
+                citiesVM.initialCitiesLoaded.append(contentsOf: nextBatch)
                 
                 isLoading = false
             }
         }
     }
-
+    
 }
 
 #Preview {
